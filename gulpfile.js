f074d50063c7c632b9bfe4f87944bbfd7549154c
Ray Villalobos <ray@planetoftheweb.com>
2020-10-15 05:15:27 -0400

    modified starter files

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  source = './public/',
+  dest = './public/';
+
+function html() {
+  return gulp.src(dest + '**/*.html');
+}
+
+function js() {
+  return gulp.src(dest + '**/*.js');
+}
+
+function styles() {
+  console.log('styles');
+  return gulp.src(dest + '**/*.css');
+}
+
+function watch() {
+  gulp.watch(source + 'js/**/*.js', js).on('change', browserSync.reload);
+  gulp.watch(source + 'css/**/*.css', styles).on('change', browserSync.reload);
+  gulp.watch(source + 'index.html', html).on('change', browserSync.reload);
+}
+
+function server() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: source,
+    },
+  });
+
+  gulp.watch(source + 'css/**/*.css', styles).on('change', browserSync.reload);
+  gulp.watch(source + 'index.html', html).on('change', browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, styles, html), server, watch);
+
+gulp.task('default', build);
