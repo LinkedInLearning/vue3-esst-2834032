b1ccddb81bd2cf9e5ec35255eb677f80dbe0af28
Ray Villalobos <ray@planetoftheweb.com>
2020-11-02 06:10:17 -0800

    mend

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-multi-spaces': ['error'],
+    'vue/no-multiple-template-root': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
